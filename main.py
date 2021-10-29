56351c15b906ae2ceefe142990afa750571c46bf
yuina-blend <moon.h14.dd2.as@gmail.com>
2021-10-29 14:27:17 +0900

    fix time

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import time
 
 date_time = DateTime()
 usb = PowerControl()
-time_now = date_time.get_time().split(':')
-
-for i in range(3):
-    time_now[i] = int(time_now[i])
 
 while True:
-    if time_now[0] == 14 and time_now[1] >= 25:
+    time_now = date_time.get_time().split(':')
+
+    for i in range(3):
+        time_now[i] = int(time_now[i])
+    if time_now[0] == 14 and time_now[1] >= 28:
         usb.usb_power_off()
     time.sleep(30)
