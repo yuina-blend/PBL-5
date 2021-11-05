b62776c9b014dcfb3a391db013bc88f428228f1e
st18133no <72228964+st18133no@users.noreply.github.com>
2021-11-05 14:00:28 +0900

    Test

--- kyusui.py
+++ kyusui.py
@@ -1,21 +1,32 @@
 import time
 
-num = 6
-i = 0
-power = 0
+class Time:
 
-for i in range(num):
-    if i==3:
-        print("停止")
-        while 1:  #on...1 off...0
-            print("電源が再びつくまで待つ")
+    #def _init_(self):
+     #   self.i = 0
+      #  self.power = 0
+       # self.num = 6
+    def kyusui(self):
+        i = 0
+        power = 0
+        num = 6
+        for i in range(num): #加湿器の時間分繰り返す
+            if i==3: #電源offを10秒ごとに確認、停止したと仮定
+                print("停止")
+                while 1:  #on...1 off...0
+                    print("電源が再びつくまで待つ")
+                    time.sleep(10)
+                    power += 0.5
+                    if power == 1:
+                        print("電源ON")
+                        break
+
             time.sleep(10)
-            power += 0.5
-            if power == 1:
-                print("電源ON")
-                break
+            i += 1
+            print(i*10)
 
-    time.sleep(10)
-    i += 1
-    
-print("終了")
+        print("終了")
+
+
+clsC = Time()
+clsC.kyusui()
