d25f2f36e03ac5edbc8417e00345b7bcf59010dc
st18013yh <st18013yh@gm.ibaraki-ct.ac.jp>
2021-11-05 15:05:42 +0900

    slcak3

--- slack.py
+++ slack.py
@@ -1,12 +1,12 @@
-import requests
-import json
-import os
 
-s = str(input())
+class slack:
+    import requests
+    import json
+    import os
+    def slack(self,s)
+        WEB_HOOK_URL = os.environ['SLACK_TOKEN']
 
-WEB_HOOK_URL = os.environ['SLACK_TOKEN']
-
-requests.post(WEB_HOOK_URL, data=json.dumps({
-    "text" : s,
-}))
-print ("ok")
+        requests.post(WEB_HOOK_URL, data=json.dumps({
+            "text" : s,
+        }))
+        print ("ok")
