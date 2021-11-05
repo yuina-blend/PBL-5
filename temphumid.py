0f7070a324aa61789248021e125af2fa0134e505
st18083ms <70875843+st18083ms@users.noreply.github.com>
2021-11-05 16:03:06 +0900

    Update temphumid.py
    クラスと関数追加したバージョン

--- temphumid.py
+++ temphumid.py
@@ -9,8 +9,25 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
-# read data using pin 14
-instance = dht11.DHT11(pin=14)
+#class
+class pin:
+    pin_num = int()
+    def __init__(self, pin):
+        self.pin = pin_num
+    def pinset(self):
+        import RPi.GPIO as GPIO
+        import dht11
+        import time
+        import datetime
+        import math
+        import RPi.GPIO as GPIO
+
+        # initialize GPIO
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.cleanup()
+        # read data using pin 14
+        instance = dht11.DHT11(self.pin_num)
 
 before_result = 0.0
 while True:
@@ -19,7 +36,14 @@ while True:
         print("Last valid input: " + str(datetime.datetime.now()))
     if abs(result.temperature - before_result) > 2.0:
        print("Temperature: %d C" % result.temperature)
+       return result.temperature
        print("Humidity: %d %%" % result.humidity)
+       return result.humidity
     before_result = result.temperature
 
     time.sleep(60)
+
+
+
+
+
