39caadff5d8658d8662c4e14c6a692c2c494071e
yuina-blend <moon.h14.dd2.as@gmail.com>
2021-11-05 16:15:11 +0900

    fix indent

--- temphumid.py
+++ temphumid.py
@@ -28,12 +28,12 @@ class pin:
         # read data using pin 14
         instance = dht11.DHT11(self.pin_num)
 
-     result = instance.read()
-     if result.is_valid():
-       print("Last valid input: " + str(datetime.datetime.now()))
-       print("Temperature: %d C" % result.temperature)
-       print("Humidity: %d %%" % result.humidity)
-       return result
+        result = instance.read()
+        if result.is_valid():
+            print("Last valid input: " + str(datetime.datetime.now()))
+            print("Temperature: %d C" % result.temperature)
+            print("Humidity: %d %%" % result.humidity)
+            return result
 
 
 
