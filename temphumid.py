484f1d0db2d35433401a29495ff132b4ac2b801e
st18083ms <70875843+st18083ms@users.noreply.github.com>
2021-11-05 16:12:49 +0900

    Update temphumid.py

--- temphumid.py
+++ temphumid.py
@@ -1,15 +1,14 @@
-# import RPi.GPIO as GPIO
-# import dht11
-# import time
-# import datetime
-# import math
+import RPi.GPIO as GPIO
+import dht11
+import time
+import datetime
+import math
 
 # # initialize GPIO
-# GPIO.setwarnings(False)
-# GPIO.setmode(GPIO.BCM)
-# GPIO.cleanup()
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
 
-#class
 class pin:
     pin_num = int()
     def __init__(self, pin):
