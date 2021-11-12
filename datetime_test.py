9b9fe4bf1a611d81cc39b4a97b21caf83305046e
yuina-blend <moon.h14.dd2.as@gmail.com>
2021-11-12 14:01:51 +0900

    renamed

---
+++ datetime_test.py
@@ -0,0 +1,13 @@
+# from slack import slack
+
+# notice = slack()
+
+# notice.slack("TEST POST")
+
+from DateTime import DateTime
+
+dt = DateTime()
+
+print(dt.get_date())
+print(dt.date_time())
+print(dt.get_date_time())
