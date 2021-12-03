39af8c684fc66fdc0be4f7c1dc45f67c2c405793
st18133no <72228964+st18133no@users.noreply.github.com>
2021-12-03 17:46:54 +0900

    時間計測訂正

--- Time_k.py
+++ Time_k.py
@@ -15,11 +15,32 @@ class Time_k:
         print(self.now)
 
     def check(self):
-        self.otime = [x1 - x2 for (x1, x2) in zip(self.now, self.start)]
+        if self.now[2] < self.start[2]:
+            self.now[2] += 60
+            self.otime[2] = self.now[2] - self.start[2]
+            self.now[1] -= 1
+            if self.now[1] < self.start[1]:
+                self.now[1] += 60
+                self.otime[1] = self.now[1] - self.start[1]
+                self.now[0] -= 1
+                self.otime[0] = self.now[0] - self.start[0]
+            else:
+                self.otime[1] = self.now[1] - self.start[1]
+                self.otime[0] = self.now[0] - self.start[0]
+
+        elif self.now[1] < self.start[1]:
+            self.now[1] += 60
+            self.otime[1] = self.now[1] - self.start[1]
+            self.now[0] -= 1
+            self.otime[0] = self.now[0] - self.start[0]
+
+        else:
+            self.otime = [x1 - x2 for (x1, x2) in zip(self.now, self.start)]
+            
         print(self.otime)
         print(self.otime[2])
 
-        if self.otime[2] > 3:
+        if self.otime[1] > 3:
             return self.otime
         else:
             return "on"
