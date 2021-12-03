34ae7651cd1a25052de7c67267436c4cbe69b18c
st18083ms <70875843+st18083ms@users.noreply.github.com>
2021-12-03 14:37:18 +0900

    return

--- temphumid.py
+++ temphumid.py
@@ -10,9 +10,10 @@ GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
 class pin:
-    pin_num = int()
     def __init__(self, pin):
+        pin_num = int(pin)
         self.pin = pin_num
+
     def pinset(self):
         import RPi.GPIO as GPIO
         import dht11
@@ -26,16 +27,11 @@ class pin:
         GPIO.setmode(GPIO.BCM)
         GPIO.cleanup()
         # read data using pin 14
-        instance = dht11.DHT11(self.pin_num)
+        instance = dht11.DHT11(self.pin)
 
         result = instance.read()
         if result.is_valid():
             print("Last valid input: " + str(datetime.datetime.now()))
             print("Temperature: %d C" % result.temperature)
             print("Humidity: %d %%" % result.humidity)
-            return result
-
-
-
-
-
+            return result.temperature, result.humidity
