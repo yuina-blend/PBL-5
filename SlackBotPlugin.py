8a3554ee79d88d93b760697678cdb7883c93f03f
st18013yh <st18013yh@gm.ibaraki-ct.ac.jp>
2021-12-17 15:10:24 +0900

    ばなな！

--- SlackBotPlugin.py
+++
@@ -1,6 +0,0 @@
-from slackbot.bot import respond_to, listen_to
-import re
-
-@respond_to('\こんにちは')
-def konnitiha(message, *something):
-    message.reply('こんにちは。')
---
+++ bolt/app.py
@@ -0,0 +1,19 @@
+import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+os.environ["http_proxy"] = "http://po.cc.ibaraki-ct.ac.jp:3128"
+# ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+# 'hello' を含むメッセージをリッスンします
+# 指定可能なリスナーのメソッド引数の一覧は以下のモジュールドキュメントを参考にしてください：
+# https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
+@app.message("hello")
+def message_hello(message, say):
+    # イベントがトリガーされたチャンネルへ say() でメッセージを送信します
+    print("うんち！")
+    say(f"ばなな!")
+
+# アプリを起動します
+if __name__ == "__main__":
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
