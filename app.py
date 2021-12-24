8515af8f177e3a0f2f76865f3a6ab695d8d79f9c
yuina-blend <moon.h14.dd2.as@gmail.com>
2021-12-24 14:15:35 +0900

    update

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ os.environ["http_proxy"] = "http://po.cc.ibaraki-ct.ac.jp:3128"
 # ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
+call_count = 0
+
 # 'hello' を含むメッセージをリッスンします
 # 指定可能なリスナーのメソッド引数の一覧は以下のモジュールドキュメントを参考にしてください：
 # https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
@@ -13,21 +15,20 @@ def message_hello(message, say):
     # イベントがトリガーされたチャンネルへ say() でメッセージを送信します
     print("うんち！")
     say(f"ばなな!")
+    call_count += 1
+    say("count = " + str(call_count))
 
 from th import pin
 p = pin(14)
 
 @app.message("!now")
 def message_now(message, say):
-    # from th import pin
-    # p = pin(14)
     global p
     temphumid = p.pinset()
     print(str(temphumid[0]), str(temphumid[1]))
     notice = "温度: " + str(temphumid[0]) + ", 湿度: " + str(temphumid[1])
     print("reqested!")
     say(notice)
-    # del p
 
 @app.message("!poweroff")
 def message_poweroff(message, say):
@@ -44,6 +45,11 @@ def message_poweron(message, say):
     usb.usb_power_on(17)
     say("電源ON")
     print("power on!")
+
+@app.message("!exit")
+def message_exit(message, say):
+    import sys
+    sys.exit()
 
 # アプリを起動します
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-from DateTime import DateTime
-from PowerControl import PowerControl
-import time
+# from DateTime import DateTime
+# from PowerControl import PowerControl
+# import time
 
-date_time = DateTime()
-usb = PowerControl()
+# date_time = DateTime()
+# usb = PowerControl()
+
+# while True:
+#     time_now = date_time.get_time().split(':')
+
+#     for i in range(3):
+#         time_now[i] = int(time_now[i])
+#     print(time_now)
+#     if time_now[0] == 14 and time_now[1] >= 40:
+#         usb.usb_power_off()
+#     time.sleep(10)
+# 
+
+import subprocess
+from subprocess import PIPE
 
 while True:
-    time_now = date_time.get_time().split(':')
-
-    for i in range(3):
-        time_now[i] = int(time_now[i])
-    print(time_now)
-    if time_now[0] == 14 and time_now[1] >= 40:
-        usb.usb_power_off()
-    time.sleep(10)
+    try:
+        proc = subprocess.run(['python3', 'app.py'], stdout=PIPE, stderr=PIPE)
+        print('reset')
+    except KeyboardInterrupt:
+        import sys
+        sys.exit()
